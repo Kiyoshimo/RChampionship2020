dc7f26b7fbae6d1c531fd44777ac2a0a10066d37
三之轮银 <jason.asuka@gmail.com>
2019-12-25 12:36:00 +0800

    0.2.2

--- ArduinoTest/SteppingMotorTest_42/SteppingMotorTest_42.ino
+++ ArduinoTest/SteppingMotorTest_42/SteppingMotorTest_42.ino
@@ -1,16 +1,16 @@
-#define EN        8       //步进电机使能端，低电平有效
+#define EN 8 //步进电机使能端，低电平有效
 
-#define Y_DIR     6       //y轴 步进电机方向控制
-#define Y_STP     3       //y轴 步进控制
-#define Y_WJ      1       //y轴 步进电机精度
+#define Y_DIR 6 //y轴 步进电机方向控制
+#define Y_STP 3 //y轴 步进控制
+#define Y_WJ 1  //y轴 步进电机精度
 
-#define X_WJ      1       //y轴 步进电机精度
+#define X_WJ 1 //y轴 步进电机精度
 
 /*
  * （跳线： 000全细分1 001细分1/2  010细分1/4  110细分1/8  111细分1/16）
  * (全细分200步一圈)
  */
- 
+
 /*
 //函数：step    功能：控制步进电机方向，步数。
 //参数：dir 方向控制, dirPin对应步进电机的DIR引脚，stepperPin 对应步进电机的step引脚， steps 步进的步数
@@ -21,25 +21,28 @@ void step(boolean dir, byte dirPin, byte stepperPin, int steps)
 {
   digitalWrite(dirPin, dir);
   delay(50);
-  for (int i = 0; i < steps; i++) {
+  for (int i = 0; i < steps; i++)
+  {
     digitalWrite(stepperPin, HIGH);
-    delayMicroseconds(1200);  
+    delayMicroseconds(1200);
     digitalWrite(stepperPin, LOW);
-    delayMicroseconds(1200);  
+    delayMicroseconds(1200);
   }
 }
 
-void setup(){//将步进电机用到的IO管脚设置成输出
+void setup()
+{ //将步进电机用到的IO管脚设置成输出
   //pinMode(X_DIR, OUTPUT); pinMode(X_STP, OUTPUT);
-  pinMode(Y_DIR, OUTPUT); pinMode(Y_STP, OUTPUT);   
+  pinMode(Y_DIR, OUTPUT);
+  pinMode(Y_STP, OUTPUT);
   //pinMode(Z_DIR, OUTPUT); pinMode(Z_STP, OUTPUT);
   pinMode(EN, OUTPUT);
   digitalWrite(EN, LOW);
 }
 
-void loop(){
+void loop()
+{
 
   step(false, Y_DIR, Y_STP, 200); //y轴电机 反转1圈，200步为1圈
   delay(100);
-
 }
--- RXH/fin/fin_arduino.ino
+++ RXH/fin/fin_arduino.ino
@@ -1,89 +1,111 @@
- #include <Servo.h>
+#include <Servo.h>
 //脚位定义
-#define EN        8       //步进电机使能端，低电平有效
-#define Y_DIR     6       //y轴 步进电机方向控制脚位
-#define Y_STP     3       //y轴 步进控制脚位
-#define Z_WJ      2       //z轴 伺服马达输出脚位
-#define I_I      4       //归零光感测器输入脚位
+#define EN 8    //步进电机使能端，低电平有效
+#define Y_DIR 6 //y轴 步进电机方向控制脚位
+#define Y_STP 3 //y轴 步进控制脚位
+#define Z_WJ 2  //z轴 伺服马达输出脚位
+#define I_I 4   //归零光感测器输入脚位
 //参数
-#define Num_round 1200     //200*6*1 走一圈步进电机的步数 （跳线： 000全细分1 001细分1/2  010细分1/4  110细分1/8  111细分1/16）(全细分200步一圈)
-int ag_ser = 0;    // Servo角度存储变量
-int ag_down =75;    // Servo下降角度
-int ag_up =45;    // Servo上升角度
+const int Num_round 1200; // 走一圈步进电机的步数200*6*1 （跳线： 000全细分1 001细分1/2  010细分1/4  110细分1/8  111细分1/16）(全细分200步一圈)
+int ag_ser = 0;           // Servo角度存储变量
+int ag_down = 75;         // Servo下降角度
+int ag_up = 45;           // Servo上升角度
 
-String mes; //串口string
-int num_pin = 6;//针的数量，默认6
-int list_wire[500];//线的list, Max5000
+String mes;                   //串口string
+int num_pin = 6;              //针的数量，默认6
+int list_wire[500];           //线的list, Max5000
+boolean light_senser = false; //光感测器度数，有1无0
 //int num_wire = 6;//绕线的次数，默认6
-Servo myservo;  // 定义Servo对象来控制
-
+Servo myservo; // 定义Servo对象来控制
 
 //步进控制：dir 方向控制, dirPin对应步进电机的DIR引脚，stepperPin 对应步进电机的step引脚， steps 步进的步数
-void step(boolean dir, byte dirPin, byte stepperPin, int steps) {
+void step(boolean dir, byte dirPin, byte stepperPin, int steps)
+{
   digitalWrite(dirPin, dir);
   delay(50);
-  for (int i = 0; i < steps; i++) {
+  for (int i = 0; i < steps; i++)
+  {
     digitalWrite(stepperPin, HIGH);
-    delayMicroseconds(800);  
+    delayMicroseconds(800);
     digitalWrite(stepperPin, LOW);
-    delayMicroseconds(800);  
+    delayMicroseconds(800);
   }
 }
 
-
-
-void setup(){
+void setup()
+{
   //将步进电机用到的IO管脚设置成输出
-  pinMode(Y_DIR, OUTPUT); 
-  pinMode(Y_STP, OUTPUT);   
+  pinMode(Y_DIR, OUTPUT);
+  pinMode(Y_STP, OUTPUT);
   pinMode(EN, OUTPUT);
   digitalWrite(EN, LOW);
   //伺服电机设置
-  myservo.attach(Z_WJ);  // 控制线连接
+  myservo.attach(Z_WJ); // 控制线连接
   //将归零光感器用到的IO管脚设置成输入
-  pinMode(I_I, INPUT);     
+  pinMode(I_I, INPUT);
 }
 
-void loop(){
+void loop()
+{
   //一、串口接收
   mes = String("");
 
-  while (Serial.available()>0){  //接收到信号----------
-  
-    char data= Serial.read();
-    mes+=data;
-    delay(2);}
-    
-  
-  if(mes!=""){  //分割数据--------------
+  while (Serial.available() > 0) //接收到信号----------
+  {
+    char data = Serial.read();
+    mes += data;
+    delay(2);
+  }
 
-    int commaPosition; 
-    int cont=0; 
-    commaPosition = mes.indexOf(',');//第一个逗号找出针的数量
-    num_pin=mes.substring(0,commaPosition).toInt();
+  if (mes != "") //分割数据--------------
+  {
 
-    for(int i=0 ; i<500 ;i++){list_wire[i]=0;} //initial
-    mes = mes.substring(commaPosition+1, mes.length());//挖走qian面的
-    
-    do{
+    int commaPosition;
+    int cont = 0;
+    commaPosition = mes.indexOf(','); //第一个逗号找出针的数量
+    num_pin = mes.substring(0, commaPosition).toInt();
+
+    for (int i = 0; i < 500; i++)
+    {
+      list_wire[i] = 0;
+    }                                                     //initial
+    mes = mes.substring(commaPosition + 1, mes.length()); //挖走qian面的
+
+    do
+    {
       commaPosition = mes.indexOf(',');
-      list_wire[cont]=mes.substring(0,commaPosition).toInt();
-      mes = mes.substring(commaPosition+1, mes.length());//挖走qian面的
-      cont++; 
-    }
-    while(commaPosition >=0); //没逗号退出
+      list_wire[cont] = mes.substring(0, commaPosition).toInt();
+      mes = mes.substring(commaPosition + 1, mes.length()); //挖走qian面的
+      cont++;
+    } while (commaPosition >= 0); //没逗号退出
     //print to debug
-    Serial.print("cont=");Serial.println(cont);
-    Serial.print("num_pin=");Serial.println(num_pin);
-     }
-
-
+    Serial.print("cont=");
+    Serial.println(cont);
+    Serial.print("num_pin=");
+    Serial.println(num_pin);
+  }
 
   //未知、基本操控（归零，步进走，舵机动，步进走，归零）
-  
-  //
 
-  step(false, Y_DIR, Y_STP, Num_round); 
-  delay(1000);
+  //歸零
+  while (1)
+  {
+    step(true, Y_DIR, Y_STP, 1); //y轴电机 zheng转1step，
+    if (digitalRead(I_I) == 1)
+    {
+      Serial.print("归零完成");
+      break;
+    }
+  }
 
+  num_pin = 13;
+  for (int i = 0; i <= 12; i++) //测试数据
+  {
+    0
+  };
+  list_wire[i] = n[i];
+}
+
+step(false, Y_DIR, Y_STP, Num_round);
+delay(1000);
 }
